5c40c4215cbe3f2a065ec44180d66997245c0066
ScratchHacks <33055857+scratchhacks@users.noreply.github.com>
2017-10-24 15:11:59 +0100

    Update hacks.js

--- hacks.js
+++ hacks.js
@@ -1,3 +1,3 @@
 function starthacks() {
-document.body.innerHTML &= "<hr><br><center><h1>ScratchHacks</h1><h3>Beta 1</h3></center>"
+document.write("<hr><br><center><h1>ScratchHacks</h1><h3>Beta 1</h3></center>");
 }
